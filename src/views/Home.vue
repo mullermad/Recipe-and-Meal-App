e7e77903502c65f2c6dd8d600a9e0d7b3decb35d
MulukenDemis <amsaledemismuller@gmail.com>
2024-02-06 23:09:38 +0300

    all setuped

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center p-8">
+   <input type="text" class="w-full rounded border-1 p-2 border-gray-200" 
+   placeholder="search for meals">
+   <div>
+
+   </div>
+   </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+
+const meals=computed(()=>store.state.meals);
+
+</script>
+
+
+
